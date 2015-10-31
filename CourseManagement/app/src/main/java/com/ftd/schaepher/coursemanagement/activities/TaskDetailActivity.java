4b1f78bb037b0d1493d9baafe2406afe67ca4904
woshidasusu <295207731@qq.com>
2015-11-01 00:02:26 +0800

    创建任务详情分支，开始开发

---
+++ CourseManagement/app/src/main/java/com/ftd/schaepher/coursemanagement/activities/TaskDetailActivity.java
@@ -0,0 +1,11 @@
+package com.ftd.schaepher.coursemanagement.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sxq on 2015/10/31.
+ * 任务详情页面
+ */
+public class TaskDetailActivity extends AppCompatActivity {
+
+}
