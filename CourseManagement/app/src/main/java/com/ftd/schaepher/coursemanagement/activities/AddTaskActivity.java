550d6412243410a6e1da1cf34e2b4800bcda46ca
woshidasusu <295207731@qq.com>
2015-10-31 21:55:43 +0800

    发布任务界面已完成，耗时1小时

---
+++ CourseManagement/app/src/main/java/com/ftd/schaepher/coursemanagement/activities/AddTaskActivity.java
@@ -0,0 +1,22 @@
+package com.ftd.schaepher.coursemanagement.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ftd.schaepher.coursemanagement.R;
+
+/**
+ * Created by sxq on 2015/10/31.
+ */
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        ActionBar mActionBar = getSupportActionBar();
+        mActionBar.setTitle("发布报课任务");
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
