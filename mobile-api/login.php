9aa43d9ffcc328a803afee7119fb05d6df676867
Schaepher <279393092@qq.com>
2015-12-01 00:35:15 +0800

    将login改为面向对象；添加login的测试，给出测试例子并测试通过

--- mobile-api/login.php
+++ mobile-api/login.php
@@ -2,26 +2,20 @@
 // 关闭错误报告
 error_reporting(0);
 
+require_once './classes/class_login.php';
+
 $workNumber = $_POST["login-user"];
 $password = $_POST["login-password"];
 $tableName = $_POST["ident"];
-session_start();
-$_SESSION['id']=$tableName;
 
-$con = mysqli_connect("localhost", "root", "", "teacher_class_system");
-if (!$con) {
-    die('Could not connect: ' . mysql_error());
-} else {
-    mysqli_query($con, "SET NAMES utf8");
+$log = new Login;
+$response = $log->login($workNumber,$password,$tableName);
 
-    $result = mysqli_query($con, "SELECT * FROM $tableName where workNumber = $workNumber and password = $password");
-    if (mysqli_num_rows($result) < 1) {
-        echo "false";
-    } else {
-        $result_arr = mysqli_fetch_assoc($result);
-        echo json_encode($result_arr, JSON_UNESCAPED_UNICODE);
-
-    }
+if($response != "false"){
+    session_start();
+    $_SESSION['id']=$tableName;
 }
+
+echo $response;
 
 ?>
